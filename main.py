66edf02d32411554826eaf22be50df8da6cfff88
phyxl <phyxl01@gmail.com>
2015-03-15 05:43:56 -0400

    made things.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -16,7 +16,10 @@ def hello():
 def show_page(page):
 	return render_template(page)
 
-
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+	
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
